71ad1f09fb9bf884443d5a44ed9945929a597f7a
Johan Morales <jhellmetal2000@gmail.com>
2015-12-09 16:16:53 -0500

    Tutorial 2 colores en python y como llenar un elemento con color.

--- tuto1.py
+++ tuto1.py
@@ -5,6 +5,12 @@ from pygame.locals import *
 #para iniciar pygame se escribe
 pygame.init()
 
+#-------------------------------------------------------------------------------------
+#Colores en pygame
+color = (0,140,60)#verde
+colorDos = pygame.Color(255,120,9)#naranja
+#-------------------------------------------------------------------------------------
+
 #para crear una ventana
 #recibe un arreglo de puntos o tupla
 ventana = pygame.display.set_mode((400,300))
@@ -12,8 +18,10 @@ ventana = pygame.display.set_mode((400,300))
 #para colocar un titulo a la ventana 
 pygame.display.set_caption("Hola mundo")
 
-#bucle infinito que se ejecuta durante toda la aplicación
+#bucle infinito que se ejecuta durante toda la aplicacion
 while True:
+	#se llena la ventana con el color hecho previamente
+	ventana.fill(colorDos)
 	#captura los eventos
 	for evento in pygame.event.get():
 		#si el usuario presiona salir
